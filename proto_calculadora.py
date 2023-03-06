2ace8f99aca40a5bffe724af31599340bc01e477
JAN URBAN NETO <jan.urbanneto@gmail.com>
2023-03-05 22:35:33 -0300

    pylint2

--- proto_calculadora.py
+++ proto_calculadora.py
@@ -20,7 +20,7 @@ operacoes = {
 
 while CONTINUAR:
 
-    result = 0
+    RESULT = 0
 
     print("Bem-vindo à calculardora! Selecione uma operação:")
     i = 0
@@ -51,21 +51,22 @@ while CONTINUAR:
         valor2 = float(input())
 
         if opcao == 0:
-            result = valor1 + valor2
+            RESULT = valor1 + valor2
 
         elif opcao == 1:
-            result = valor1 - valor2
+            RESULT = valor1 - valor2
 
         elif opcao == 2:
-            result = valor1 * valor2
+            RESULT = valor1 * valor2
 
         elif opcao == 3:
-            result = valor1 / valor2
+            RESULT = valor1 / valor2
 
         elif opcao == 4:
-            result = valor1 ** valor2
+            RESULT = valor1 ** valor2
 
-        print(f"\nResultado da operação: {valor1} {op_string} {valor2} = {result}\n")
+        print("\nResultado da operação:"
+              f"{valor1} {op_string} {valor2} = {RESULT}\n")
 
     else:
         print("Valor inválido! Tente novamente.\n")
