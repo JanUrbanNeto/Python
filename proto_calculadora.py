c177eb9e17ac831583f703297182bcfae7181f51
JAN URBAN NETO <jan.urbanneto@gmail.com>
2023-03-05 22:31:47 -0300

    pylint sugestions

--- proto_calculadora.py
+++ proto_calculadora.py
@@ -1,11 +1,13 @@
-continuar = True
+CONTINUAR = True
 
-def isNumber(value):
+
+def is_number(value):
     try:
-         float(value)
+        float(value)
     except ValueError:
-         return False
+        return False
     return True
+
 
 operacoes = {
     "+": "Soma",
@@ -16,33 +18,32 @@ operacoes = {
     "5": "sair"
 }
 
-while continuar:
-    
+while CONTINUAR:
+
     result = 0
 
     print("Bem-vindo à calculardora! Selecione uma operação:")
     i = 0
     for op, nome in operacoes.items():
         print(i, "-", nome)
-        i += 1    
+        i += 1
 
     print("")
 
     opcao = input()
 
-    if isNumber(opcao):
-        
+    if is_number(opcao):
         opcao = int(opcao)
 
         op_string = list(operacoes.keys())[opcao]
 
         if opcao == 5:
-            continuar = False
+            CONTINUAR = False
             print("\nSaindo da calculadora\n")
             break
 
-        print("A operação escolhida foi {}\n".format(op_string))
-        
+        print(f"A operação escolhida foi {op_string}\n")
+
         print("digite o primeiro valor:")
         valor1 = float(input())
 
@@ -63,8 +64,8 @@ while continuar:
 
         elif opcao == 4:
             result = valor1 ** valor2
-        
-        print("\nResultado da operação: {} {} {} = {}\n".format(valor1, op_string, valor2 ,result))
+
+        print(f"\nResultado da operação: {valor1} {op_string} {valor2} = {result}\n")
 
     else:
-        print("Valor inválido! Tente novamente.\n")       
+        print("Valor inválido! Tente novamente.\n")
